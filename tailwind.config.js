67736dfe1a691e9de908397318010d1af8ebcd66
Franklin <franklinokolie86@gmail.com>
2021-02-05 15:24:05 +0100

    Added custom color and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'Green': '#3B99A0',
+        'litgreen': '#E6F5F6'
+      },
+      fontFamily:{
+        'open': ['Open sans', 'san serif']
+      }
+    },
   },
   variants: {
     extend: {},
